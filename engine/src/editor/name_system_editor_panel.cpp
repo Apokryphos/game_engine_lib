a70e68ac6d53b22ac6490e5188a4e86da9ab916b
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-07-10 19:41:32 -0400

    Fixed issue with name component editor panel corrupting entity names.

--- engine/src/editor/name_system_editor_panel.cpp
+++ engine/src/editor/name_system_editor_panel.cpp
@@ -16,11 +16,14 @@ void NameSystemEditorPanel::on_update(Game& game, const ecs::Entity entity) {
     const auto name_cmpnt = name_sys.get_component(entity);
     const std::string& name = name_sys.get_name(name_cmpnt);
 
-    const unsigned MAX_NAME_LEN = 64;
+    const size_t MAX_NAME_LEN = 64;
     std::array<char, MAX_NAME_LEN> str_array;
-    name.copy(str_array.data(), MAX_NAME_LEN);
-    ImGui::InputText("Name", str_array.data(), MAX_NAME_LEN);
-    const std::string& new_name = str_array.data();
-    name_sys.set_name(name_cmpnt, new_name.data());
+    str_array.fill(0);
+    std::copy(name.begin(), name.end(), str_array.data());
+    // name.copy(str_array.data(), std::min(name.length()+1, MAX_NAME_LEN));
+    if (ImGui::InputText("Name", str_array.data(), str_array.size())) {
+        const std::string new_name = str_array.data();
+        name_sys.set_name(name_cmpnt, new_name);
+    }
 }
 }
