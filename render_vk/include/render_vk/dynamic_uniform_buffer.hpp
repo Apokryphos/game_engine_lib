2207e54cd843c78e10326b62639020752eb906ff
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-07-13 14:12:08 -0400

    Moved ubo_dynamic functions into dynamic_uniform_buffer files.

--- render_vk/include/render_vk/dynamic_uniform_buffer.hpp
+++ render_vk/include/render_vk/dynamic_uniform_buffer.hpp
@@ -156,4 +156,13 @@ public:
         return m_buffer;
     }
 };
+
+//  ----------------------------------------------------------------------------
+void get_dynamic_buffer_align_and_size(
+    VkPhysicalDevice physical_device,
+    const size_t object_count,
+    const size_t uniform_buffer_size,
+    size_t& dynamic_align,
+    size_t& buffer_size
+);
 }
