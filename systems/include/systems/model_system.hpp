57eded9af5ab5665e1bef7851356b1e8af1e16d4
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-07-15 16:06:00 -0400

    Added texture ID to model system components.

--- systems/include/systems/model_system.hpp
+++ systems/include/systems/model_system.hpp
@@ -8,10 +8,14 @@ namespace systems
 struct ModelComponentData
 {
     uint32_t id;
+    uint32_t texture_id;
 
     template <typename Archive>
     void archive(Archive& ar) {
-        ar(id);
+        ar(
+            id,
+            texture_id
+        );
     }
 };
 
@@ -26,10 +30,18 @@ public:
         return get_component_data(cmpnt).id;
     }
 
+    uint32_t get_texture_id(const Component cmpnt) const {
+        return get_component_data(cmpnt).texture_id;
+    }
+
     static const common::SystemId Id = SYSTEM_ID_MODEL;
 
     void set_id(const Component cmpnt, const uint32_t id) {
         get_component_data(cmpnt).id = id;
+    }
+
+    void set_texture_id(const Component cmpnt, const uint32_t texture_id) {
+        get_component_data(cmpnt).texture_id = texture_id;
     }
 };
 }
