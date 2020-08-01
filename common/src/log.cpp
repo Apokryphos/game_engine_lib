1390651e09fa7ec2825808654e1f35f80e69a71e
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-08-01 12:48:44 -0400

    Added timestamp to top of log file.

--- common/src/log.cpp
+++ common/src/log.cpp
@@ -1,4 +1,6 @@
 #include "common/log.hpp"
+#include <chrono>
+#include <iomanip>
 
 namespace common
 {
@@ -10,6 +12,14 @@ void initialize_log(const std::string& path) {
     log->file.open(path);
 
     if (log->file.good()) {
+        //  Add timestamp at top of log file
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t timestamp = std::chrono::system_clock::to_time_t(now);
+        log->file <<
+            "Log file created " <<
+            std::put_time(std::localtime(&timestamp), "%F %T") <<
+            ".\n";
+
         s_log = std::move(log);
     } else {
         log_error("Failed to open log file '%s'.", path.c_str());
