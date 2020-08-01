44742dccf1abd578127431d0bc92cd3eccf79974
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-08-01 09:28:05 -0400

    Added trace to VK_CHECK_RESULT macro.

--- render_vk/include/render_vk/vulkan.hpp
+++ render_vk/include/render_vk/vulkan.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "common/trace.hpp"
 #include "volk.h"
 #include <cassert>
 
 #define VK_CHECK_RESULT(f)          \
 {                                   \
+    common::Trace::entry(#f);       \
     VkResult result = (f);          \
     assert(result == VK_SUCCESS);   \
+    common::Trace::exit(#f);        \
 }
